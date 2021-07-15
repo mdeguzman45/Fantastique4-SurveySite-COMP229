e3e6b4d2026ecfdf09ca2ac4e5fd32ae12b470e1
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-07-15 02:59:21 -0400

    Added module dependencies to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('fantastique4-surveysite:server');
 var http = require('http');
 
