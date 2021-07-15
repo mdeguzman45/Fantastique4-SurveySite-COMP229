6cebafd6fd75fc5d326e7231d36aeaf9846f6a73
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-07-15 03:47:29 -0400

    Configured Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET create survey page. */
+router.get('/create', function(req, res, next) {
+  res.render('index', { title: 'Create a Survey' });
+});
+
+/* GET my survey page. */
+router.get('/mysurvey', function(req, res, next) {
+  res.render('index', { title: 'My Surveys' });
+});
+
+/* GET open surveys page. */
+router.get('/open', function(req, res, next) {
+  res.render('index', { title: 'Open Surveys' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login' });
+});
+
+
 
 module.exports = router;
