806c422f46b6854cf874c3d4e2bdd2eb03f2bae9
Rushita192 <rushitapatel112@gmail.com>
2021-07-17 02:45:31 +0530

    Created Sign up from

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-<%- include partials/header.ejs %>
+const express = require('express')
+const app = express()
+const port = 3000
 
-<%- include partials/main_nav.ejs %>
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
 
-  <main class="center">
-    <p class="lead" style="font-size: 3em;">Welcome to</p>
-
-    <img src="/assets/images/F4-New.png" class="img-fluid" alt="" width="800" height="100">
-    <p class="lead" style="font-size: 3em;">Survey Site</p>
-
-    <br />
-    <a href="list/add" class="btn btn-danger shadow-none custom-button">TAKE A SURVEY</a>
-  </main>
-
-<%- include partials/bottom_nav.ejs %>
-
-<%- include partials/footer.ejs %>
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
