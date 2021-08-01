6022757ec269811ff61efe81d34e0bae3f76b551
Rushita192 <rushitapatel112@gmail.com>
2021-08-01 17:21:10 +0530

    Added path to routes folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,10 @@ let indexController = require('../controllers/index');
 /* GET home page. */
 router.get('/', indexController.displayHomePage);
 
-/* GET my survey page. */
-router.get('/mysurvey', function(req, res, next) {
-  res.render('index', { title: 'My Surveys' });
-});
+// /* GET my survey page. */
+// router.get('/mysurvey', function(req, res, next) {
+//   res.render('index', { title: 'My Surveys' });
+// });
 
 /* Get Route for displaying the Login page */
 router.get('/login', indexController.displayLoginPage);
@@ -34,6 +34,10 @@ router.get('/logout', indexController.performLogout);
 // router.get('/edit', function(req, res, next) {
 //   console.log('[routes.index.edit] rendering edit survey page');
 //   res.render('partials/edit-survey', { title: 'Edit a Survey' });
+// });
+
+// router.get('/take', function(req, res, next) {
+//   res.render('partials/take-survey', { title: 'Take a Survey' });
 // });
 
 module.exports = router;
--- server/routes/survey.js
+++ server/routes/survey.js
@@ -17,7 +17,7 @@ function requireAuth(req, res, next) {
 let surveyController = require('../controllers/survey');
 
 /* GET open surveys page. */
-router.get('/', requireAuth, surveyController.displayOpenSurveys);
+router.get('/', surveyController.displayOpenSurveys);
 
 /* GET create survey page. */
 router.get('/add', requireAuth, surveyController.displayAddSurveyPage);
@@ -25,16 +25,22 @@ router.get('/add', requireAuth, surveyController.displayAddSurveyPage);
 /* POST create survey page */
 router.post('/add', requireAuth, surveyController.processAddSurvey);
 
+/* GET create survey page. */
+router.get('/mysurvey', requireAuth, surveyController.displayMySurveyPage);
+
+/* GET Take survey page. */
+router.get('/take/:id', surveyController.displayTakeSurveyPage);
+
+/* GET Take survey page. */
+router.post('/take/:id', surveyController.processTakeSurveyPage);
+
+
 //edit the survey
 router.get('/edit/:id', requireAuth, surveyController.displayEditSurveyPage);
 
 //update the survey 
 router.post('/edit/:id', requireAuth, surveyController.processEditSurvey);
 
-//take survey
-router.get('/take', requireAuth, function(req, res, next) {
-    res.render('partials/take-survey', { title: 'Take a Survey' });
-});
 
 //delete the survey
 router.get('/delete/:id', requireAuth, surveyController.deleteSurvey);
